1055a3657218ee73f50d18024ccfb78ccc43b6af
Aziri-Dev <71698364+Aziri-Dev@users.noreply.github.com>
2023-02-03 08:29:38 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,29 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AuthModule } from '@auth0/auth0-angular';
+import config from '../../capacitor.config';
+import {LoginButtonComponent} from "./login/login-button/login-button.component";
+import {LogoutButtonComponent} from "./login/logout-button/logout-button.component";
+
+// Build the URL that Auth0 should redirect back to
+const redirect_uri = `${config.appId}://dev-3adocs3bconafo8d.us.auth0.com/capacitor/${config.appId}/callback`;
+
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  declarations: [AppComponent, LoginButtonComponent, LogoutButtonComponent],
+  entryComponents: [],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AuthModule.forRoot({
+      domain: "dev-3adocs3bconafo8d.us.auth0.com",
+      clientId: "YCYOaF66WycinTG2leQKhMqRZT8bWu4o",
+      authorizationParams: {
+        redirect_uri,
+      }
+    }),
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
---
+++ src/app/login/logout-button/logout-button.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+import { Browser } from '@capacitor/browser';
+import { tap } from 'rxjs/operators';
+import config from "../../../../capacitor.config";
+
+// Build the URL to return back to your app after logout
+const returnTo = `${config.appId}://dev-3adocs3bconafo8d.us.auth0.com/capacitor/${config.appId}/callback`;
+
+@Component({
+  selector: 'app-logout-button',
+  template: `<ion-button (click)="logout()">Log out</ion-button>`,
+})
+export class LogoutButtonComponent {
+  // Import the AuthService module from the Auth0 Angular SDK
+  constructor(public auth: AuthService) {}
+
+  logout() {
+    this.auth
+      .logout({
+        logoutParams: {
+          returnTo,
+        },
+        async openUrl(url: string) {
+          await Browser.open({ url });
+        }
+      })
+      .subscribe();
+  }
+}
