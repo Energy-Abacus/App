e2d358e5b95e3b3995196fe39862948e05ae27b3
Elias Gstallnig <e.gstallnig@gmail.com>
2023-12-01 09:25:22 +0100

    merge

--- src/app/plugs/plugs-routing.module.ts
+++ src/app/plugs/plugs-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {MatNativeDateModule} from '@angular/material/core'
 
 import { PlugsPage } from './plugs.page';
 
--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -6,9 +6,7 @@ import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../components/explore-container/explore-container.module';
 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
-import {NgChartsModule} from "ng2-charts";
 import {NgApexchartsModule} from "ng-apexcharts";
-import {Tab2PageModule} from "../tab2/tab2.module";
 import { MenuComponent } from '../components/menu/menu.component';
 import { LoginButtonComponent } from '../components/login/login-button/login-button.component';
 import { LogoutButtonComponent } from '../components/login/logout-button/logout-button.component';
@@ -18,7 +16,6 @@ import { LogoutButtonComponent } from '../components/login/logout-button/logout-
     IonicModule,
     CommonModule,
     FormsModule,
-    NgChartsModule,
     NgApexchartsModule,
     ExploreContainerComponentModule,
     Tab1PageRoutingModule,
